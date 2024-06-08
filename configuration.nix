05cb7a466cd9478b98592359163037ff9fd38426
Diego Pereyra <diesphink@gmail.com>
2024-06-08 00:49:52 -0300

    Don't ask for password on sudo for nixos-rebuild switch

--- configuration.nix
+++ configuration.nix
@@ -221,6 +221,13 @@
     }
   ];
 
+  security.sudo = {
+    enable = true;
+    extraConfig = ''
+      sphink ford = (root) NOPASSWD: /run/current-system/sw/bin/nixos-rebuild
+    '';
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
