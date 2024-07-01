b8c29d24ccf7421ca5055d7113be81e1bb0c1a5d
Diego Pereyra <diesphink@gmail.com>
2024-07-01 12:08:19 -0300

    Update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -61,7 +61,7 @@
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
-    driSupport = true;
+    # driSupport = true;
     driSupport32Bit = true;
     # extraPackages = with pkgs; [
     #   mesa
