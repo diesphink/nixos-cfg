6f8d2f8c323a58a90ad89a67dab54418e7baf544
Diego Pereyra <diesphink@gmail.com>
2024-09-22 10:43:35 -0300

    libvirtd

--- configuration.nix
+++ configuration.nix
@@ -141,6 +141,23 @@
   ];
 
   virtualisation.docker.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
+  };
 
   programs.file-roller.enable = true;
 
@@ -190,6 +207,7 @@
       "networkmanager"
       "wheel"
       "docker"
+      "libvirtd"
     ];
   };
 
